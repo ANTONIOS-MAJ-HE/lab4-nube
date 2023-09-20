7b7b1d856073830cfb00e57bbf77f23aeda71d2b
ANTONIOS-MAJ-HE <antonio.santiago@tecsup.edu.pe>
2023-09-20 13:13:22 -0500

    primer commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.get('/', (req, res) => {
+  res.send('¡Hola, mundo!');
+});
+
+app.get('/clientes', (req, res) => {
+  const clientes = ['Cliente 1', 'Cliente 2', 'Cliente 3'];
+  res.json(clientes);
+});
+
+app.get('/productos', (req, res) => {
+  const productos = ['Producto 1', 'Producto 2', 'Producto 3'];
+  res.json(productos);
+});
+
+app.listen(port, () => {
+  console.log(`La aplicación está escuchando en el puerto ${port}`);
+});
